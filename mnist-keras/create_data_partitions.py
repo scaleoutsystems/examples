2e70a28e470ff7d749a09c74ca521266b2ec6457
Fredrik Wrede <fredrik@scaleoutsystems.com>
2021-09-22 13:00:09 +0200

    bug fix and verbose

--- mnist-keras/create_data_partitions.py
+++ mnist-keras/create_data_partitions.py
@@ -18,13 +18,14 @@ if __name__ == '__main__':
     if len(sys.argv) < 2:
         nr_of_datasets = 10
     else:
-        nr_of_datasets = sys.argv[1]
+        nr_of_datasets = int(sys.argv[1])
 
     package = np.load("data/mnist.npz")
     data = {}
     for key, val in package.items():
         data[key] = splitset(val, nr_of_datasets)
 
+    print("CREATING {} PARTITIONS INSIDE {}/data/clients".format(nr_of_datasets, os.getcwd()))
     if not os.path.exists('data/clients'):
         os.mkdir('data/clients')
 
@@ -36,3 +37,4 @@ if __name__ == '__main__':
                 y_train=data['y_train'][i],
                 x_test=data['x_test'][i],
                 y_test=data['y_test'][i])
+    print("DONE")
