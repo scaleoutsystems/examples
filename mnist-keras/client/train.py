7684ec0ec1f1924985d27d58c02d7e9a7a33b15d
Fredrik Wrede <fredrik@scaleoutsystems.com>
2021-10-11 11:42:32 +0200

    remove caching

--- mnist-keras/client/train.py
+++ mnist-keras/client/train.py
@@ -12,21 +12,7 @@ import os
 def train(model,data,settings):
     print("-- RUNNING TRAINING --", flush=True)
 
-    # We are caching the partition in the container home dir so that
-    # the same training subset is used for each iteration for a client.
-    try:
-        x_train = np.load('/tmp/local_dataset/x_train.npz')
-        y_train = np.load('/tmp/local_dataset/y_train.npz')
-    except:
-        (x_train, y_train) = read_data(data, trainset=True)
-
-        try:
-            os.mkdir('/tmp/local_dataset')
-            np.save('/tmp/local_dataset/x_train.npz',x_train)
-            np.save('/tmp/local_dataset/y_train.npz',y_train)
-
-        except:
-            pass
+    (x_train, y_train) = read_data(data, trainset=True)
 
     model.fit(x_train, y_train, batch_size=settings['batch_size'], epochs=settings['epochs'], verbose=1)
 
