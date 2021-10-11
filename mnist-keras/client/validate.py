eb5a9c6044d51b00190c36483e73e786fd6927be
Fredrik Wrede <fredrik@scaleoutsystems.com>
2021-10-11 11:43:15 +0200

    remove caching in validate.py

--- mnist-keras/client/validate.py
+++ mnist-keras/client/validate.py
@@ -11,40 +11,14 @@ import numpy as np
 def validate(model,data):
     print("-- RUNNING VALIDATION --", flush=True)
 
-    # The data, split between train and test sets. We are caching the partition in
-    # the container home dir so that the same data subset is used for
-    # each iteration.
-
+    # The data, split between train and test sets.
 
     # Training error (Client validates global model on same data as it trains on.)
-    try:
-        x_train = np.load('/tmp/local_dataset/x_train.npz')
-        y_train = np.load('/tmp/local_dataset/y_train.npz')
-    except:
-        (x_train, y_train) = read_data(data, trainset=True)
-        try:
-            os.mkdir('/tmp/local_dataset')
-            np.save('/tmp/local_dataset/x_train.npz', x_train)
-            np.save('/tmp/local_dataset/y_train.npz', y_train)
-
-        except:
-            pass
+    (x_train, y_train) = read_data(data, trainset=True)
 
     # Test error (Client has a small dataset set aside for validation)
-    try:
-        x_test = np.load('/tmp/local_dataset/x_test.npz')
-        y_test = np.load('/tmp/local_dataset/y_test.npz')
-    except:
-        (x_test, y_test) = read_data(data, trainset=False)
-        try:
-            os.mkdir('/tmp/local_dataset')
-            np.save('/tmp/local_dataset/x_test.npz', x_test)
-            np.save('/tmp/local_dataset/y_test.npz', y_test)
-
-        except:
-            pass
-
-
+    (x_test, y_test) = read_data(data, trainset=False)
+     
     try:
         model_score = model.evaluate(x_train, y_train, verbose=0)
         print('Training loss:', model_score[0])
