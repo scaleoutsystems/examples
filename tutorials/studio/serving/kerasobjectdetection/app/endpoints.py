bfa001ac17bffe5fae215f8ab4df3c196cbe03b4
TechCarmat <carmat88@users.noreply.github.com>
2021-10-22 12:09:08 +0200

    Update endpoints.py: removing specific urls
    Making it a tiny bit more general

--- tutorials/studio/serving/kerasobjectdetection/app/endpoints.py
+++ tutorials/studio/serving/kerasobjectdetection/app/endpoints.py
@@ -1,9 +1,9 @@
 
 # Configure serving endpoints
 endpoints = {
-    'inceptionv3': 'https://r41f7d1ac.studio.scaleoutsystems.com/v1/models/models:predict',
-    'resnet50': 'https://rba99be30.studio.scaleoutsystems.com/v1/models/models:predict',
-    'xception': 'https://rdbc9d018.studio.scaleoutsystems.com/v1/models/models:predict'
+    'inceptionv3': 'https://<your-studio-serving-app-url>/v1/models/models:predict',
+    'resnet50': 'https://<your-studio-serving-app-url>/v1/models/models:predict',
+    'xception': 'https://<your-studio-serving-app-url>/v1/models/models:predict'
 }
 
 
