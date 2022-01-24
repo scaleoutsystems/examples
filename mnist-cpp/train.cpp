d266d38f8f804cbaf61c427aa3937fbf64587c2e
mcapuccini <marco@scaleoutsystems.com>
2022-01-24 14:23:36 +0000

    Binaries working

--- mnist-cpp/train.cpp
+++ mnist-cpp/train.cpp
@@ -1,40 +1,27 @@
 #include <torch/torch.h>
+#include "net.h"
 
-#define DROPOUT 0.5
 #define BATCH_SIZE 64
 #define N_EPOCHS 10
-
-// Define neural network
-struct Net : torch::nn::Module {
-  torch::nn::Linear fc1{nullptr}, fc2{nullptr}, fc3{nullptr};
-
-  Net() {
-    fc1 = register_module("fc1", torch::nn::Linear(784, 64));
-    fc2 = register_module("fc2", torch::nn::Linear(64, 32));
-    fc3 = register_module("fc3", torch::nn::Linear(32, 10));
-  }
-
-  torch::Tensor forward(torch::Tensor x) {
-    x = torch::relu(fc1->forward(x.reshape({x.size(0), 784})));
-    x = torch::dropout(x, /*p=*/DROPOUT, /*train=*/is_training());
-    x = torch::relu(fc2->forward(x));
-    x = torch::log_softmax(fc3->forward(x), /*dim=*/1);
-    return x;
-  }
-};
+#define LEARNING_RATE 0.01
 
 int main(int argc, char** argv) {
   // Init model
   std::string out_path;
   std::shared_ptr<Net> net = std::make_shared<Net>();
-  if (argc == 3) {
-    torch::load(net, argv[1]);
-    out_path = argv[2];
+  if (argc == 3) { // if 3 args
+    torch::load(net, argv[1]); // load from arg 1
+    out_path = argv[2]; // save to arg 2
   } else if (argc == 2) {
-    out_path = argv[1];  
+    out_path = argv[1]; // save to arg 1, no initial model
   } else {
     std::cerr << "Wrong number of arguments" << std::endl;
+    exit(1);
   }
+
+  // Get other params from environment vars
+  size_t n_splits =  std::stoi(std::getenv("N_SPLITS"));
+  size_t split =  std::stoi(std::getenv("SPLIT"));
 
   // Multi-threaded data loader for the MNIST dataset.
   auto data_loader = torch::data::make_data_loader(
@@ -43,10 +30,7 @@ int main(int argc, char** argv) {
         BATCH_SIZE);
 
   // Init optimizer
-  torch::optim::SGD optimizer(net->parameters(), /*lr=*/0.01);
-
-  size_t n_splits =  std::stoi(std::getenv("N_SPLITS"));
-  size_t split =  std::stoi(std::getenv("SPLIT"));
+  torch::optim::SGD optimizer(net->parameters(), LEARNING_RATE);
 
   // Train loop
   for (size_t epoch = 1; epoch <= N_EPOCHS; ++epoch) { // epoch loop
@@ -59,8 +43,10 @@ int main(int argc, char** argv) {
         loss.backward(); // backprop
         optimizer.step(); // update params
         // Print logs
-        std::cerr << "Epoch: " << epoch << " | Batch: " << batch_index
-                  << " | Loss: " << loss.item<float>() << std::endl;
+        if(batch_index % (100 + split) == 0) {
+          std::cerr << "Epoch: " << epoch << " | Batch: " << batch_index
+                    << " | Loss: " << loss.item<float>() << std::endl;
+        }
       }
       batch_index++;
     }
