82f9de39d6165a8fc40a98bbbae01169d8c2bda2
mcapuccini <marco@scaleoutsystems.com>
2022-01-24 15:13:39 +0000

    build package

--- mnist-cpp/bin/build.sh
+++ mnist-cpp/bin/build.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
-/bin/cmake --build /examples/mnist-cpp/build --config Debug --target all -j 14 --
+# Configure
+cmake --no-warn-unused-cli \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE \
+    -DCMAKE_BUILD_TYPE:STRING=Debug \
+    -DCMAKE_C_COMPILER:FILEPATH=/usr/bin/x86_64-linux-gnu-gcc-11 \
+    -DCMAKE_CXX_COMPILER:FILEPATH=/usr/bin/x86_64-linux-gnu-g++-11 \
+    -H$PWD \
+    -B$PWD/build \
+    -G "Unix Makefiles"
+
+# Build
+cmake --build $PWD/build --config Debug --target all -j $(nproc) --
+
+# Copy binaries to right folder
+cp build/train build/validate client
+
+# Make package
+mkdir -p package
+tar -czvf package/package.tar.gz client
