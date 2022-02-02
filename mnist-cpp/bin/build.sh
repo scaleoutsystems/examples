63821813e73a64fbf235b669549e2cafeb2dd026
mcapuccini <marco@scaleoutsystems.com>
2022-02-02 15:11:31 +0000

    generage gramine files

--- mnist-cpp/bin/build.sh
+++ mnist-cpp/bin/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+
 # Configure
 cmake --no-warn-unused-cli \
     -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE \
@@ -7,7 +9,7 @@ cmake --no-warn-unused-cli \
     -DCMAKE_CXX_COMPILER:FILEPATH=/usr/bin/x86_64-linux-gnu-g++-11 \
     -H$PWD \
     -B$PWD/build \
-    -G "Unix Makefiles"
+    -G Ninja
 
 # Build
 cmake --build $PWD/build --config Debug --target all -j $(nproc) --
@@ -22,3 +24,18 @@ tar -czvf package/package.tar.gz client
 # Make seed
 SPLIT=0 N_SPLITS=1 build/train seed.pt
 python client/helper.py pt2np seed.pt seed
+
+# Generate sig key if necessary
+if [ ! -e "$HOME/.gramine/enclave-key.pem" ]; then
+    mkdir -p $HOME/.gramine
+    openssl genrsa -3 -out $HOME/.gramine/enclave-key.pem 3072
+fi
+
+# Generate SGX-related files
+pushd client
+for src in train validate; do
+    gramine-manifest -Dlog_level=debug $src.manifest.template $src.manifest
+    gramine-sgx-sign --key $HOME/.gramine/enclave-key.pem --manifest $src.manifest --output $src.manifest.sgx
+    gramine-sgx-get-token --output $src.token --sig $src.sig
+done
+popd
--- mnist-cpp/bin/launch.sh
+++ mnist-cpp/bin/launch.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 # Build container
 docker build \
